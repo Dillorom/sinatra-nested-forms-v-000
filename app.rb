ce5bb0014bf2a20240f588f3e875bd39b123facc
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-06 19:04:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,35 +4,25 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-    get '/' do
-      erb :root
-    end
-
-    get '/new' do
-      erb :'pirates/new'
-    end
-
-      # params[:pirate][:ships].each do |ship|
-      #   Ship.new(ship)
-      # end
-      # @ships = Ship.all
-
-      #erb :'pirates/show'
-
-    get "/pirates/:id" do
-      @pirate = Pirate.find(params[:id])
-      erb :"/pirate/show"
-    end
-
-    post '/pirates' do
-      pirate = Pirate.create(name: params[:pirate][:name], weight: params[:pirate][:weight], height: params[:pirate][height])
-      params[:pirate][:ships].each do |ship_data|
-        ship = Ship.new(ship_data)
-        ship.pirate = pirate
-        ship.save
-      end
-      redirect to "/pirates/#{pirate.id}"
-    end
+    get '/' do 
+      erb :root 
+    end 
+    
+    get '/new' do 
+      erb :"pirates/new"
+    end 
+    
+    post '/pirates' do 
+      @pirates = Pirate.new(params[:pirate])
+      
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end 
+      
+      @ships = Ship.all 
+      
+      erb :"pirates/show"
+    end 
 
   end
 end
